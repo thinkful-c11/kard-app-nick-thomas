629244755557f6d34d305f97ee19f0a6f1f4e37a
tckastanek <tckastanek@gmail.com>
2017-07-11 16:39:28 -0400

    Added plus icon.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,6 +8,7 @@ export default class NavBar extends React.Component {
       <div className='nav-bar'>
         <nav>
           <a href='#account'><img src='./man.svg' alt='Account Icon' /></a>
+          <a href='#add-stuff'><img src='./if_add_cross_new_plus_create_392530.svg' alt='Add Icon' /></a>
         </nav>
       </div>
     );
