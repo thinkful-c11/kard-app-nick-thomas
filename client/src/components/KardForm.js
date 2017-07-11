ad4893e28e28c13da16717d1f8723f3b85035580
tckastanek <tckastanek@gmail.com>
2017-07-11 16:33:59 -0400

    Buggy but we can email card information.

--- client/src/components/KardForm.js
+++ client/src/components/KardForm.js
@@ -25,6 +25,14 @@ export default class KardForm extends React.Component {
 
   }
   render () {
+    // const email = () => {
+    //   for (var [key, value] of this.state) {
+    //     // return `${key}: ${value}`;
+    //     console.log(`${key}: ${value}`);
+    //   }
+    // };
+    const email = Object.entries(this.state).toString().split(',');
+    console.log(email);
     return (
       <div className='kard-form-container'>
         <h1>Kard</h1>
@@ -42,7 +50,7 @@ export default class KardForm extends React.Component {
 
             <button type='submit' className='send-button'>
               <a
-                href='mailto:tckastanek@gmail.com'>Send!</a>
+                href={`mailto:tckastanek@gmail.com?subject="Here is my Kard!"&body=${email}`}>Send!</a>
             </button>
           </fieldset>
         </form>
