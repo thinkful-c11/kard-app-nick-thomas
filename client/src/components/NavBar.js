a8dfa27890b33fcbfb134aee5c137ccaa521d300
tckastanek <tckastanek@gmail.com>
2017-07-12 11:41:38 -0400

    Added send toggle on account screen.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,12 +9,13 @@ export default class NavBar extends React.Component {
   }
 
   render () {
+    const accountLink = <Link to={'/account/'}><img src='./man.svg' alt='Account Icon' /></Link>;
     const addItemLink = <Link to={'/add-item/'}><img src='./if_add_cross_new_plus_create_392530.svg' alt='Add Icon' /></Link>;
     const sendKardLink = <Link to={'/'}><img className='send-icon' src='../plane_send.svg' alt='Send Icon' /></Link>;
     return (
       <div className='nav-bar'>
         <nav>
-          <Link to={'/account/'}><img src='./man.svg' alt='Account Icon' /></Link>
+          {(this.props.match.url === '/account/') ? sendKardLink : accountLink}
           {(this.props.match.url === '/add-item/') ? sendKardLink : addItemLink}
         </nav>
       </div>
