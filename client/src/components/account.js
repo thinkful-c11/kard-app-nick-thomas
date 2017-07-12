8b8993607a91f61c83e7f57d3742d5762ac56192
tckastanek <tckastanek@gmail.com>
2017-07-12 16:10:43 -0400

    Added content to list items and wrapped icons in buttons.

--- client/src/components/account.js
+++ client/src/components/account.js
@@ -1,45 +1,66 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
-import './account.css'
+import './account.css';
 
 export class Account extends React.Component {
   render () {
 
 
     const data = this.props.kard.work.map((editcontent, index) => {
-     return (
-     <div className="account" key={'work' + index}>
-            <li>
-              {editcontent.type} <img src="../edit2.svg.png" /><img src="../delete.png" />
-            </li>
-    </div>
-     )
+      return (
+        <div className='account' key={'work' + index}>
+          <li>
+            <h3>
+              {editcontent.type}
+            </h3>
+            <p>
+              {editcontent.content}
+            </p>
+            <button><img src='../edit2.svg.png' /></button>
+            <button><img src='../delete.png' /></button>
+          </li>
+        </div>
+      );
     });
     const data2 = this.props.kard.social.map((editcontent, index) => {
-     return (
-     <div className="account" key={'social' + index}>
-            <li>
-              {editcontent.type} <img src="../edit2.svg.png" /><img src="../delete.png" />
-            </li>
-    </div>
-     )
+      return (
+        <div className='account' key={'social' + index}>
+          <li>
+            <h3>
+              {editcontent.type}
+            </h3>
+            <p>
+              {editcontent.content}
+            </p>
+            <button><img src='../edit2.svg.png' /></button>
+            <button><img src='../delete.png' /></button>
+          </li>
+        </div>
+      );
     });
     const data3 =this.props.kard.contact.map((editcontent, index) => {
-     return (
-     <div className="account" key={'contact' + index}>
-            <li>
-              {editcontent.type} <img src="../edit2.svg.png" /><img src="../delete.png" />
-            </li>
-    </div>
-     )
+      return (
+        <div className='account' key={'contact' + index}>
+          <li>
+            <h3>
+              {editcontent.type}
+            </h3>
+            <p>
+              {editcontent.content}
+            </p>
+            <button><img src='../edit2.svg.png' /></button>
+            <button><img src='../delete.png' /></button>
+          </li>
+        </div>
+      );
     });
     return (
       <div className='editContent'>
         <ul>
-        {data}
-        {data2}
-        {data3}
+          {data}
+          {data2}
+          {data3}
         </ul>
       </div>
     );
