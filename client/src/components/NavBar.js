4d5ede1c2d3924bd69f702d577d8bfd6d36870c2
tckastanek <tckastanek@gmail.com>
2017-07-12 11:49:26 -0400

    Cleaned up the dot logic.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,7 +9,7 @@ export default class NavBar extends React.Component {
   }
 
   render () {
-    const whichDot = (this.props.match.url === '/account/' || this.props.match.url === '/add-item/') ? '..' : '.';
+    const whichDot = (this.props.match.url === '/') ? '.' : '..';
     const accountLink = <Link to={'/account/'}><img src={`${whichDot}/man.svg`} alt='Account Icon' /></Link>;
     const addItemLink = <Link to={'/add-item/'}><img src={`${whichDot}/if_add_cross_new_plus_create_392530.svg`} alt='Add Icon' /></Link>;
     const sendKardLink = <Link to={'/'}><img className='send-icon' src='../plane_send.svg' alt='Send Icon' /></Link>;
