e63e4b52e39b323eae16d6a8fd3e96638eaa4e4e
tckastanek <tckastanek@gmail.com>
2017-07-14 13:41:44 -0400

    Working on nav bar css.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -16,18 +16,6 @@ export default class NavBar extends React.Component {
     (this.state.showNav) ? this.setState({showNav: false}) : this.setState({showNav: true});
   }
   render () {
-    // const whichDot = (this.props.match.url === '/') ? '.' : '..';
-    // const accountLink = <Link to={'/account/'}><img className='editIcon' src={`${whichDot}/edit2.svg.png`} alt='Edit Icon' /></Link>;
-    // const addItemLink = <Link to={'/add-item/'}><img src={`${whichDot}/if_add_cross_new_plus_create_392530.svg`} alt='Add Icon' /></Link>;
-    // const sendKardLink = <Link to={'/'}><img className='send-icon' src='../plane_send.svg' alt='Send Icon' /></Link>;
-    // return (
-    //   <div className='nav-bar'>
-    //     <nav>
-    //       {(this.props.match.url === '/account/') ? sendKardLink : accountLink}
-    //       {(this.props.match.url === '/add-item/') ? sendKardLink : addItemLink}
-    //     </nav>
-    //   </div>
-    // );
     const hidden = (this.state.showNav) ? 'hidden' : '';
 
     return (
@@ -40,10 +28,10 @@ export default class NavBar extends React.Component {
 
 
         <nav id='main-nav' className={`main-nav ${hidden}`} >
-          <ul className='main-nav-container flex-container'>
+          <ul className='main-nav-container'>
+            <li className='main-nav-link'><Link to={'/'}>Home</Link></li>
             <li className='main-nav-link'><Link to={'/account/'}>Edit Items</Link></li>
             <li className='main-nav-link'><Link to={'/add-item/'}>Add Item</Link></li>
-            <li className='main-nav-link'><Link to={'/'}>Home</Link></li>
           </ul>
         </nav>
       </div>
